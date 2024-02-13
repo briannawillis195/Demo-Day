6cbdf70ea48bea0eaf7c5c1ecde28c019104ef88
Brianna Willis <brianna.willis195@gmail.com>
2024-02-13 11:36:11 -0500

    fix format

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts")
+const connectDB = require("./server/config/database");
+const mainRoutes = require("./server/routes/main");
+const postRoutes = require("./server/routes/posts")
 
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
-require("./config/passport")(passport);
+require("./server/config/passport")(passport);
 
 //Connect To Database
 connectDB();
